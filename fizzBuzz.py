9c673ad90e0d0ead41bd64f77b1639f2396bf19a
frisqia fatmalaningsih <frifani123@gmail.com>
2024-05-29 06:43:38 +0700

    fizzBuzz

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -16,17 +16,19 @@ def fizzbuzz(n: int) -> list:
     - fizzbuzz(15) should return ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]
     """
     # Implement your solution here
-    result = []
-    for i in range(1, n + 1):
-        if i % 3 == 0 and i % 5 == 0:
+    if n == 1:
+        return ["1"]
+    else:
+        result = fizzbuzz(n - 1)
+        if n % 3 == 0 and n % 5 == 0:
             result.append("FizzBuzz")
-        elif i % 3 == 0:
+        elif n % 3 == 0:
             result.append("Fizz")
-        elif i % 5 == 0:
+        elif n % 5 == 0:
             result.append("Buzz")
         else:
-            result.append(str(i))
-    return result
+            result.append(str(n))
+        return result
 
 
 # You can test your function with print statements below
